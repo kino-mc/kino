723f9ec6e083f116cb1f63dab88ba5116f880e88
Adrien Champion <adrien.champion@mail.com>
2015-12-24 16:30:11 -0600

    [tig] Evaluator for some of the term constructors

---
+++ tig/src/eval.rs
@@ -0,0 +1,169 @@
+// Copyright 2015 Adrien Champion. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::collections::HashMap ;
+use std::marker::PhantomData ;
+
+use term::{ Offset2, Cst, Term, Factory, Model } ;
+
+use graph::{ Key, Val } ;
+
+
+/** Cached evaluator.
+
+For big systems, the cache of the evaluator can grow quite big. Note that we
+always evaluate the same set of terms, hence its size is the same between
+iterations.
+
+Ideally we want to allocate it, reach the max capacity, and reuse that cache
+when evaluating over a different model to avoid re-allocating it. That's why
+the workflow is to create one evaluator and then recycle it:
+
+`let evaluator = Eval::mk(model_1, offset_1, factory) ;`
+
+Do stuff, and later just recycle.
+
+`evaluator.recycle(new_model, new_offset) ;`
+*/
+struct Eval<V: Val> {
+  /// Phantom data for the type we evaluate things to.
+  phantom: PhantomData<V>,
+  /// The model we're evaluating with.
+  model: Model,
+  /// The offset we're evaluating with.
+  offset: Offset2,
+  /// The cache for term evaluation in this model.
+  cache: HashMap<Key, Cst>,
+  /// Term factory for actual evaluation.
+  factory: Factory,
+}
+impl<V: Val> Eval<V> {
+  /** Builds a new evaluator. Only call once, then call `recycle` for optimal
+  cache allocation. */
+  pub fn mk(model: Model, offset: Offset2, factory: Factory) -> Self {
+    Eval {
+      phantom: PhantomData,
+      model: model, offset: offset,
+      cache: HashMap::with_capacity(100), factory: factory
+    }
+  }
+
+  /** Resets the evaluator with a new model. The cache is reset but its
+  capacity is kept. */
+  pub fn recycle(& mut self, model: Model, offset: Offset2) {
+    self.model = model ;
+    self.offset = offset ;
+    self.cache.clear() ;
+    ()
+  }
+
+  /// Evaluates a term, cached.
+  pub fn eval(& mut self, term: Term) -> Result<V, String> {
+    use term::zip::Step ;
+    let mut stack = vec![] ;       // Vector of `Step`s for zipping down/up.
+    let mut values  = vec![] ;     // Vector of arguments already evaluated.
+    let mut current = vec![term] ; // Vector of terms for current arguments.
+
+    loop {
+
+      'eval_args: loop {
+
+        if let Some(term) = current.pop() {
+          // There is a term to evaluate for this step.
+
+          if let Some(cst) = self.cache.get(& term.hkey()) {
+            // Cache hit, done with this term.
+            values.push(cst.clone())
+
+          } else {
+            use term::real_term::Term::* ;
+            // Need to evaluate this term.
+            match * term.get() {
+              // Variable, evaluate.
+              V(_) => match self.factory.eval(
+                & term, & self.offset, & self.model
+              ) {
+                Ok(c) => values.push(c),
+                Err(s) => return Err(s),
+              },
+
+              // Constant, nothing to do.
+              C(ref cst) => values.push(cst.clone()),
+
+              // Operator, putting on stack.
+              Op(ref op, ref args) => {
+                use std::iter::Extend ;
+                stack.push(
+                  (
+                    Step::Op(op.clone(), vec![()]),
+                    values, current
+                  )
+                ) ;
+                values = Vec::with_capacity(args.len()) ;
+                current = Vec::with_capacity(args.len()) ;
+                current.extend( args.iter().map(|t| t.clone()) )
+              },
+
+              App(ref sym, _) => panic!(
+                format!("evaluation of applications not implemented ({})", sym)
+              ),
+
+              Let(_, _) => panic!(
+                "evaluation of let bindings not implemented"
+              ),
+
+              Forall(_, _) | Exists(_, _) => panic!(
+                "evaluation of quantifier not implemented"
+              ),
+            }
+          }
+        } ;
+
+        if current.is_empty() {
+          // Done evaluating the arguments of whatever step we're in.
+          // Going up now.
+          break 'eval_args
+        }
+      }
+
+      // Going up. There should no term to evaluate.
+      debug_assert!( current.is_empty() ) ;
+
+      if let Some( (step, vals, curs) ) = stack.pop() {
+        // Stack's not empty.
+        match step {
+
+          Step::Op(op, _) => {
+            // Evaluating operator on arguments.
+            let val = match op.eval(& self.factory, values) {
+              Ok(val) => val,
+              Err(s) => return Err(s),
+            } ;
+            // Restoring previous context.
+            values = vals ;
+            current = curs ;
+            // Pushing new value to context.
+            values.push(val)
+          },
+
+          _ => panic!("aaa"),
+        }
+      } else {
+        // Stack is empty, there should be exactly one value in `values` and no
+        // term in `current`. We're done.
+        debug_assert!(  values.len() == 1 ) ;
+        debug_assert!( current.is_empty() ) ;
+        let val = values.pop().unwrap() ;
+        return V::eval_cst(
+          & val, & self.offset, & self.model, & self.factory
+        )
+      }
+    }
+  }
+}
---
+++ tig/src/graph.rs
@@ -0,0 +1,359 @@
+// Copyright 2015 Adrien Champion. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::fmt ;
+use std::cmp::Ord ;
+use std::hash::{ Hash, Hasher } ;
+use std::collections::{ HashSet, HashMap } ;
+use std::marker::PhantomData ;
+
+use term::{
+  Offset2, Cst, Term, Operator, Bool, Int, Rat, Factory, Model
+} ;
+
+
+
+
+
+
+/// Key representing a node, corresponds to the key of its representant.
+pub type Key = u64 ;
+
+
+
+
+
+/// The value trait provides the operator encoding the ordering.
+pub trait Val: Sized + Ord + Eq + Hash {
+  /// The operator encording the ordering, e.g. `=>` for `Bool`.
+  fn order_op() -> Operator ;
+  /// Evaluates a term to some value given a model.
+  fn eval(
+    & Term, & Offset2, & Model, & Factory
+  ) -> Result<Self, String> ;
+  /// Evaluates a constant to some value given a model.
+  fn eval_cst(
+    cst: & Cst, off: & Offset2, mdl: & Model, factory: & Factory
+  ) -> Result<Self, String> {
+    use term::CstMaker ;
+    let cst = factory.cst(cst) ;
+    Self::eval(& cst, off, mdl, factory)
+  }
+}
+
+impl Val for Bool {
+  fn order_op() -> Operator { Operator::Impl }
+  fn eval(
+    term: & Term, offset: & Offset2, model: & Model, factory: & Factory
+  ) -> Result<Bool, String> {
+    factory.eval_bool(term, offset, model)
+  }
+}
+impl Val for Int {
+  fn order_op() -> Operator { Operator::Le }
+  fn eval(
+    term: & Term, offset: & Offset2, model: & Model, factory: & Factory
+  ) -> Result<Int, String> {
+    factory.eval_int(term, offset, model)
+  }
+}
+impl Val for Rat {
+  fn order_op() -> Operator { Operator::Le }
+  fn eval(
+    term: & Term, offset: & Offset2, model: & Model, factory: & Factory
+  ) -> Result<Rat, String> {
+    factory.eval_rat(term, offset, model)
+  }
+}
+
+
+
+
+
+
+/// A node in the invariant generation graph.
+pub struct Node<V> {
+  /// Phantom data for the type the terms evaluate to.
+  phantom: PhantomData<V>,
+  /// Term representing the class.
+  rep: Term,
+  /// All terms of the node except `rep`.
+  others: HashSet<Term>,
+  /// The keys of the nodes directly above this one.
+  above: HashSet<Key>,
+  /// Stores the nodes potentially above this one after the split.
+  above_transient: HashSet<Key>,
+  /// The keys of the nodes directly below this one.
+  below: HashSet<Key>,
+}
+
+impl<V> Hash for Node<V> {
+  fn hash<H: Hasher>(& self, state: & mut H) {
+    state.write_u64(self.rep.hkey())
+  }
+}
+
+impl<V> fmt::Display for Node<V> {
+  fn fmt(& self, fmt: & mut fmt::Formatter) -> fmt::Result {
+    write!(
+      fmt, "{}[{}]({}<{}<{})",
+      self.rep.hkey(), self.rep.get(),
+      self.above.len(), self.others.len(), self.below.len()
+    )
+  }
+}
+
+impl<V: Val> Node<V> {
+  /// Creates a new node.
+  #[inline(always)]
+  pub fn mk(rep: Term, others: HashSet<Term>) -> Self {
+    debug_assert!(
+      ! others.contains(& rep)
+    ) ;
+    Node {
+      phantom: PhantomData,
+      rep: rep,
+      others: others,
+      above: HashSet::new(),
+      above_transient: HashSet::new(),
+      below: HashSet::new(),
+    }
+  }
+  /// Creates a new node with a representant and no other terms.
+  #[inline(always)]
+  pub fn of_rep(rep: Term) -> Self {
+    Self::mk( rep, HashSet::new() )
+  }
+
+  /// The key of the node (hkey of the representant).
+  #[inline(always)]
+  pub fn key(& self) -> Key { self.rep.hkey() }
+
+  /// Adds a term to the node.
+  #[inline(always)]
+  pub fn insert(& mut self, term: Term) -> bool {
+    self.others.insert(term)
+  }
+
+  /// The nodes above this one.
+  #[inline(always)]
+  pub fn above(& self) -> & HashSet<Key> { & self.above }
+  /// The nodes below this one.
+  #[inline(always)]
+  pub fn below(& self) -> & HashSet<Key> { & self.below }
+
+  /** Splits a node based on a model `mdl`.
+
+  Returns the nodes `N_1`, ..., `N_n` such that
+
+  * all elements of `N_i` are equal to some value `v_i` under `mdl`,
+  * for `0 <= i < n`, `v_i \le v_{i+1}` where `\le` is the ordering on the
+    values.
+
+  Uses `insert_in_vec`. */
+  pub fn split(
+    self, model: & Model, offset: & Offset2, factory: & Factory
+  ) -> Result<Vec<(V,Self)>, String> {
+    debug_assert!( self.above.is_empty() ) ;
+    debug_assert!( self.below.is_empty() ) ;
+    let mut vec = vec![] ;
+    match V::eval(& self.rep, offset, model, factory) {
+      Ok(v) => vec.push( (v, Self::of_rep(self.rep)) ),
+      Err(s) => return Err(s),
+    } ;
+    for term in self.others.into_iter() {
+      match V::eval(& term, offset, model, factory) {
+        Ok(v) => vec = insert_in_vec(vec, v, term),
+        Err(s) => return Err(s),
+      }
+    } ;
+    Ok(vec)
+  }
+}
+
+/** Helper function for `split`, inserts a value / term pair in a sorted
+value / node pair.
+
+Inserts the term in the node corresponding to the value if any, otherwise
+creates a new node with this term as representative. */
+fn insert_in_vec<V: Val>(
+  vec: Vec<(V, Node<V>)>, val: V, term: Term
+) -> Vec<(V, Node<V>)> {
+  use std::cmp::Ordering::* ;
+  use std::iter::Extend ;
+  let mut new = Vec::with_capacity(vec.len() + 1) ;
+  let mut old = vec.into_iter() ;
+  loop {
+    if let Some( (v, mut node) ) = old.next() {
+      match v.cmp(& val) {
+        Less => new.push( (v, node) ),
+        Equal => {
+          node.insert(term) ;
+          new.push( (v, node) ) ;
+          break
+        },
+        Greater => {
+          new.push( (v, node) ) ;
+          new.push( (val, Node::of_rep(term)) ) ;
+          break
+        },
+      }
+    } else {
+      new.push( (val, Node::of_rep(term)) ) ;
+      break
+    }
+  }
+  new.extend(old) ;
+  new
+}
+
+
+
+
+
+
+
+/// A graph is some roots and some nodes.
+struct Graph<V> {
+  /// Roots of the graph.
+  roots: HashSet<Key>,
+  /// Nodes of the graph.
+  nodes: HashMap<Key, Node<V>>,
+}
+
+impl<V: Val> Graph<V> {
+  /// The node and value associated with a key.
+  #[inline(always)]
+  pub fn node(& self, index: & Key) -> Option<& Node<V>> {
+    self.nodes.get(index)
+  }
+  /// The node and value associated with a key (mutable).
+  #[inline(always)]
+  pub fn node_mut(
+    & mut self, index: & Key
+  ) -> Option<& mut Node<V>> {
+    self.nodes.get_mut(index)
+  }
+
+  /** Extracts a root. Removes it from the set of nodes and returns the root
+  and its kids. */
+  #[inline]
+  pub fn root(& mut self) -> Option<Node<V>> {
+    let root = match self.roots.iter().next() {
+      Some(key) => * key,
+      None => return None,
+    } ;
+    let was_there = self.roots.remove(& root) ;
+    debug_assert!( was_there ) ;
+    match self.nodes.remove(& root) {
+      Some( root ) => Some(root),
+      None => panic!(
+        "node {} is registered as root but unknown to set of nodes",
+        root
+      )
+    }
+  }
+
+  /// Splits a graph according to a model.
+  pub fn split(
+    mut self, model: & Model, offset: & Offset2, factory: & Factory
+  ) -> Result<Self, String> {
+    // Orphan kids, kids we can process.
+    let mut orphans = Vec::with_capacity(13) ;
+    // The new graph we're building.
+    let mut new = Graph {
+      roots: HashSet::with_capacity(self.roots.len()),
+      nodes: HashMap::with_capacity(self.nodes.len()),
+    } ;
+
+    // Handle all roots first.
+    'roots: loop {
+      if let Some(root) = self.root() {
+        let old_key = root.key() ;
+
+        // Splitting root.
+        let mut ordered = match root.split(model, offset, factory) {
+          Ok(ordered) => ordered, Err(s) => return Err(s)
+        } ;
+
+        // Adding biggest node, i.e. the new root.
+        let mut prev = if let Some((_, root)) = ordered.pop() {
+          let key = root.key() ;
+          new.roots.insert(key) ;
+          new.nodes.insert(key, root) ;
+          key
+        } else {
+          return Err(
+            format!("split of root {} resulted in nothing", old_key)
+          )
+        } ;
+
+        // Looping over what's below the new root.
+        'nu_kids: loop {
+          if let Some((_, mut kid)) = ordered.pop() {
+            kid.above.insert(prev) ;
+            match new.nodes.get_mut(& prev) {
+              Some(prev) => prev.below.insert(kid.key()),
+              None => return Err(
+                format!("unknown previous node {}", prev)
+              ),
+            } ;
+            prev = kid.key() ;
+            let was_there = new.nodes.insert(kid.key(), kid) ;
+            debug_assert!(was_there.is_none())
+          } else {
+            break 'nu_kids
+          }
+        } ;
+
+        // Removing the root from its kids, adding smallet new node as
+        // transient. Remembering orphan kids.
+        for kid in new.nodes.get_mut(& old_key).unwrap().above().iter() {
+          match self.nodes.get_mut(kid) {
+            Some(kid) => {
+              let was_there = kid.above.remove(& old_key) ;
+              debug_assert!(was_there) ;
+              let was_there = kid.above_transient.insert(prev) ;
+              debug_assert!(! was_there) ;
+              if kid.above.is_empty() {
+                orphans.push(kid.key()) // Orphan, remember it.
+              }
+            },
+            None => return Err(
+              format!("unknown root with key {}", kid)
+            ),
+          }
+        } ;
+        ()
+      } else { break 'roots }
+    } ;
+
+    // 'non_roots: loop {
+    //   if let Some(node) = orphans.pop() {
+    //     let old_key = node.key() ;
+
+    //     // Split node.
+    //     let mut ordered = match node.split(model, offset, factory) {
+    //       Ok(ordered) => ordered,
+    //       Err(s) => return Err(s),
+    //     } ;
+
+    //     // Updating biggest node.
+    //     let mut prev = if let Some((_, node)) = ordered.pop() {
+    //       let key = node.key() ;
+    //       new.nodes.insert
+    //     }
+
+
+    //   } else { break 'non_roots }
+    // } ;
+
+    Ok(new)
+  }
+}
